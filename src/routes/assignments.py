84bca94cabe949364e423fbbb3ebf77201cdbd8e
MantequillaCraft <ponceyahir395@gmail.com>
2025-09-17 00:51:24 -0600

    [FEAT] delete Assignment endpoint

--- src/routes/assignments.py
+++ src/routes/assignments.py
@@ -7,14 +7,14 @@ router = APIRouter()
 
 @router.get("/get_assignment")
 async def get_assignment(
-    class_id:str= Query(...)
+    assignment_id:str= Query(...)
 ):
     try:
         supabase = setup.supabase
         clases = (
             supabase.table("assignments")
             .select("*")
-            .eq("id", class_id)
+            .eq("id", assignment_id)
             .execute()
         )
         return JSONResponse(clases.data[0])
@@ -29,9 +29,9 @@ async def get_assignment(
 async def get_all_assignments():
     try:
         supabase = setup.supabase
-        clases = supabase.table("assignments").select("*").execute()
-        
-        return JSONResponse(clases.data)
+        assignments = supabase.table("assignments").select("*", count="exact").execute()
+
+        return JSONResponse({ "data" : { "total" : assignments.count },"something":assignments.data})
     except Exception as e:
         return JSONResponse (
             content={"error" : str(e)},
@@ -44,14 +44,16 @@ async def add_assignment(
     auth_token:str = Header(...),
     assignment_title:str= Query(...)
 ):
-    general.authentication(auth_token=auth_token)
-
+    user_data, _ = general.authentication(auth_token=auth_token)
+    code = general.sanitize_string(text=assignment_title)
     try:
         supabase = setup.supabase
         clases = (
             supabase.table("assignments")
             .insert({
-                "title" : assignment_title
+                "code" : code,
+                "title" : assignment_title,
+                "created_by" : user_data["id"]
             })
             .execute()
         )
@@ -60,6 +62,43 @@ async def add_assignment(
             "message" : "Assignmente create Succesfully ",
             "assignment_id" : clases.data[0]["id"]
         })
+    except Exception as e:
+        return JSONResponse (
+            content={"error" : str(e)},
+            status_code=500
+        )
+
+
+@router.delete("/delete_assignment")
+async def delete_assignment(
+    auth_token:str = Header(...),
+    assignment_id:str= Query(...)
+):
+    user_data, _ = general.authentication(auth_token=auth_token)
+    try:
+        supabase = setup.supabase
+        a = (
+            supabase.table("assignments")
+            .select("title","created_by")
+            .eq("id", assignment_id)
+            .execute()
+        )
+
+        if user_data["id"] == a.data[0]["created_by"] or user_data["role"]=="admin":
+            response = (
+                supabase.table("assignments")
+                .delete()
+                .eq("id", assignment_id)
+                .execute()
+            )
+
+            return JSONResponse({
+                "message" : "Assignmente deleted Succesfully ",
+                "assignment" : a.data[0]["title"]
+            })
+        else:
+            return HTTPException(status_code=401, detail="Error, you have no permissions")
+        
     except Exception as e:
         return JSONResponse (
             content={"error" : str(e)},
--- src/utils/general.py
+++ src/utils/general.py
@@ -1,9 +1,9 @@
 import jwt
 import os
+import re
 from dotenv import load_dotenv
 from src.utils.setup import supabase
 from fastapi import HTTPException
-from fastapi.responses import JSONResponse
 
 load_dotenv()
 
@@ -26,3 +26,11 @@ def authentication(auth_token: str):
         return user.data[0], decoded #user.data[0] = data del usuario, decoded = payload decoded
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Something went wrong: {e}")
+
+
+def sanitize_string(text: str) -> str:
+    # Reemplaza cualquier cosa que NO sea a-z, A-Z, 0-9 por "_"
+    sanitized = re.sub(r'[^a-zA-Z0-9]', '_', text)
+
+    # Convierte todo a minúsculas
+    return sanitized.lower()
